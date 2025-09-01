75c3e106ab953d00c91a48f299f92830792584dd
VeronikaVS90 <vsokolovanika@gmail.com>
2025-09-01 23:16:54 +0300

    draft for product info

--- src/components/ProductInfo/ProductInfo.tsx
+++ src/components/ProductInfo/ProductInfo.tsx
@@ -3,6 +3,13 @@ import type { IProduct } from "../../types/products";
 import ProductForm from "../ProductForm/ProductForm";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { productSchema, type ProductFormType } from "../ProductForm/lib";
+import {
+  Box,
+  Button,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+} from "@mui/material";
 
 interface ProductInfoProps {
   product: IProduct;
@@ -20,8 +27,42 @@ export default function ProductInfo({ product }: ProductInfoProps) {
   });
 
   return (
-    <div>
-      <ProductForm disabled form={form} />
-    </div>
+    <>
+      <DialogTitle
+        sx={{
+          fontWeight: "bold",
+          fontSize: "1.25rem",
+          textAlign: "start",
+          textTransform: "uppercase",
+          pb: 1,
+        }}
+      >
+        Product id:
+      </DialogTitle>
+      <DialogContent>
+        <Box>
+          <div>Created at:</div>
+          <div>Rating:</div>
+          <ProductForm disabled form={form} />
+        </Box>
+      </DialogContent>
+      <DialogActions sx={{ justifyContent: "space-between" }}>
+        <Button type="button" variant="outlined" sx={{ borderRadius: 2 }}>
+          Go back
+        </Button>
+
+        <Box sx={{ display: "flex", gap: 1 }}>
+          <Button variant="contained" sx={{ borderRadius: 2 }}>
+            Update
+          </Button>
+          <Button variant="outlined" sx={{ borderRadius: 2 }}>
+            Reset
+          </Button>
+          <Button variant="contained" color="error" sx={{ borderRadius: 2 }}>
+            Delete
+          </Button>
+        </Box>
+      </DialogActions>
+    </>
   );
 }
