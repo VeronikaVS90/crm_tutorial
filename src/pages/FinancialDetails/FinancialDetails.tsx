17735d4151b831015493e9c5fb941e3d58b4313f
VeronikaVS90 <vsokolovanika@gmail.com>
2025-09-14 22:44:39 +0300

    financial info refactoring

--- src/pages/FinancialDetails/FinancialDetails.tsx
+++ src/pages/FinancialDetails/FinancialDetails.tsx
@@ -1,11 +1,19 @@
 import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router";
-import type { IFinance, IUpdateFinanceBody } from "../../types/financial";
+import { useForm, type SubmitHandler } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import type { IFinance } from "../../types/financial";
 import { financialService } from "../../shared/services/financial";
 import { handleError } from "../../shared/services/errorHandler";
 import CircularIndeterminate from "../../components/Loader/Loader";
-import FinancialInfo from "../../components/FinancialInfo";
+import {
+  financialSchema,
+  type FinancialFormType,
+} from "../../components/FinancialForm/lib";
 import { financialStore } from "../../shared/store/financial";
+import FinancialForm from "../../components/FinancialForm";
+import { FinanceReadonlyInfo } from "../../components/ui";
+import { ButtonActions } from "../../components/ui";
 
 export default function FinancialDetails() {
   const { financeId = "" } = useParams();
@@ -14,6 +22,14 @@ export default function FinancialDetails() {
   const [finance, setFinance] = useState<IFinance | null>(null);
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [error, setError] = useState<unknown>();
+  const [editMode, setEditMode] = useState(false);
+  const [isUpdating, setIsUpdating] = useState(false);
+  const [isDeleting, setIsDeleting] = useState(false);
+
+  const form = useForm<FinancialFormType>({
+    resolver: yupResolver(financialSchema),
+    defaultValues: finance || {},
+  });
 
   const fetchFinance = async (financeId: string) => {
     try {
@@ -21,6 +37,15 @@ export default function FinancialDetails() {
       setIsLoading(true);
       const finance = await financialService.getFinanceById(financeId);
       setFinance(finance);
+      form.reset({
+        month: finance.month,
+        year: finance.year,
+        income: finance.income,
+        outcome: finance.outcome,
+        type: finance.type,
+        transactions: finance.transactions,
+        comment: finance.comment,
+      });
     } catch (error) {
       setError(error);
       handleError(
@@ -32,20 +57,40 @@ export default function FinancialDetails() {
     }
   };
 
-  const handleUpdateFinance = async (data: IUpdateFinanceBody) => {
-    await financialStore.updateFinance(data);
-    await fetchFinance(data.id);
+  const handleUpdateFinance: SubmitHandler<FinancialFormType> = async (
+    data
+  ) => {
+    if (!finance) return;
+
+    try {
+      setIsUpdating(true);
+      await financialStore.updateFinance({ ...data, id: finance.id });
+      await fetchFinance(finance.id);
+      setEditMode(false);
+    } catch (error) {
+      handleError(
+        error,
+        "Failed to update calculation. Please, try again later."
+      );
+    } finally {
+      setIsUpdating(false);
+    }
   };
 
-  const handleDeleteFinance = async (financeId: string) => {
+  const handleDeleteFinance = async () => {
+    if (!finance) return;
+
     try {
-      await financialStore.deleteFinance(financeId);
+      setIsDeleting(true);
+      await financialStore.deleteFinance(finance.id);
       navigate("/financial", { replace: true });
     } catch (error) {
       handleError(
         error,
         "Failed to delete calculation. Please, try again later."
       );
+    } finally {
+      setIsDeleting(false);
     }
   };
 
@@ -60,10 +105,32 @@ export default function FinancialDetails() {
   if (!finance) return null;
 
   return (
-    <FinancialInfo
-      finance={finance}
-      onUpdateFinance={handleUpdateFinance}
-      onDeleteFinance={handleDeleteFinance}
-    />
+    <>
+      <FinanceReadonlyInfo finance={finance} />
+
+      <FinancialForm disabled={!editMode || isUpdating} form={form} />
+
+      <ButtonActions
+        editMode={editMode}
+        isUpdating={isUpdating}
+        isDeleting={isDeleting}
+        onEnableEdit={() => setEditMode(true)}
+        onReset={() => {
+          setEditMode(false);
+          form.reset({
+            month: finance.month,
+            year: finance.year,
+            income: finance.income,
+            outcome: finance.outcome,
+            type: finance.type,
+            transactions: finance.transactions,
+            comment: finance.comment,
+          });
+        }}
+        onSave={form.handleSubmit(handleUpdateFinance)}
+        onDelete={handleDeleteFinance}
+        onGoBack={() => navigate("/financial")}
+      />
+    </>
   );
 }
