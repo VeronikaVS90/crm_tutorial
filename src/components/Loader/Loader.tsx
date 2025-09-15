da0b11a3e2a87ed6eb2128a3a0e664ed8679c8cf
VeronikaVS90 <vsokolovanika@gmail.com>
2025-09-15 23:58:07 +0300

    update loader

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -3,7 +3,7 @@ import Box from "@mui/material/Box";
 
 export default function CircularIndeterminate() {
   return (
-    <Box sx={{ display: "flex" }}>
+    <Box sx={{ display: "flex", justifyContent: "center" }}>
       <CircularProgress />
     </Box>
   );
