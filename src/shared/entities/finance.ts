303e850ce68b75714864ec457fceac601633604e
pm.bovios <pm.bovios@gmail.com>
2025-09-16 10:30:58 +0300

    add financial entities

---
+++ src/shared/entities/finance.ts
@@ -0,0 +1,28 @@
+import type { IFinance, FinanceMonth, TransactionType } from "../../types/financial";
+
+export class Finance {
+  id: string;
+  month: FinanceMonth;
+  year: number;
+  type: TransactionType;
+  transactions: number;
+  income: number;
+  outcome: number;
+  profit: number;
+  averageCheck: number;
+  comment: string;
+
+  constructor(finance: IFinance) {
+    this.id = finance.id;
+    this.month = finance.month;
+    this.year = finance.year;
+    this.type = finance.type;
+    this.transactions = finance.transactions;
+    this.income = finance.income;
+    this.outcome = finance.outcome;
+    this.profit = finance.profit;
+    this.averageCheck = finance.averageCheck;
+    this.comment = finance.comment;
+  }
+
+}
