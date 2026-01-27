0cfa4f90cd061bc17e33cfcd17717a22170845ad
VeronikaVS90 <vsokolovanika@gmail.com>
2026-01-27 15:47:37 +0200

    feat(customers): show notes in list table

--- src/pages/Customers/lib/constants.ts
+++ src/pages/Customers/lib/constants.ts
@@ -3,10 +3,16 @@ import type { ITableColumn } from "../../../components/Table/lib";
 import type { ICustomerResponse } from "../../../types/customers";
 
 export const customerColumns: ITableColumn<ICustomerResponse>[] = [
-    { id: "id", label: "Id" },
-    { id: "name", label: "Name" },
-    { id: "email", label: "Email" },
-    { id: "phone", label: "Phone" },
-    { id: "company", label: "Company", cell: ({ company }) => company ?? "-", },
-    { id: "createdAt", label: "Created At", cell: ({ createdAt }) => dayjs(createdAt).format("DD.MM.YYYY, HH:mm"), },];
+  { id: "id", label: "Id" },
+  { id: "name", label: "Name" },
+  { id: "email", label: "Email" },
+  { id: "phone", label: "Phone" },
+  { id: "company", label: "Company", cell: ({ company }) => company ?? "-" },
+  { id: "notes", label: "Notes", cell: ({ notes }) => notes ?? "-" },
+  {
+    id: "createdAt",
+    label: "Created At",
+    cell: ({ createdAt }) => dayjs(createdAt).format("DD.MM.YYYY, HH:mm"),
+  },
+];
 
